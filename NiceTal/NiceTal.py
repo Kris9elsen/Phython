3f51ac3509e642a2dd10f28b21a0182b03135db2
Kris9elsen <kristoffer9elsen@gmail.com>
2022-03-03 12:16:59 +0100

    Create NiceTal.py

---
+++ NiceTal/NiceTal.py
@@ -0,0 +1,13 @@
+from pickle import FALSE, TRUE
+
+
+times = input("Enter amount of times to add: ")
+times = int(times)
+res = 1
+i = 0
+
+
+while i < times:
+    print(i + 1, " : ",  res)
+    res = res * 2
+    i = i + 1
