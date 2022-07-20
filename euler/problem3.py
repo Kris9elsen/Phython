d9f980e9c413a70273cbcc1fdad349ef723a73cc
Kris9elsen <kristoffer9elsen@gmail.com>
2022-07-20 15:54:44 +0200

    Problem 3 on going

--- euler/problem3.py
+++ euler/problem3.py
@@ -1,15 +1,19 @@
 num = "6,0,0,8,5,1,4,7,5,1,4,3"
-
 numList = num.split(",")
 
-for x in numList:
-    print(x)
-
-for x in range(len(numList)-1):
-    c = numList[x] + numList[x+1]
-    print(c)
+nums = []
 
 for x in range(len(numList)):
-    sum = ""
-    for i in range(x):
-        sum = numList[i]
+    for i in range(len(numList)):
+        n = ""
+        h = i
+        for y in range(x):
+            n += numList[h]
+            h += 1
+            if (len(n) == x):
+                n = int(n)
+                nums.append(n)
+            if (h > len(numList)-1):
+                break
+nums.sort(reverse = True)
+print(nums)
