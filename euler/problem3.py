3a32ac3d2bb8132838f85a75d030b6f3b954bfe2
Kris9elsen <kristoffer9elsen@gmail.com>
2022-07-21 21:10:20 +0200

    Update problem3.py

--- euler/problem3.py
+++ euler/problem3.py
@@ -1,42 +1,15 @@
-num = "6,0,0,8,5,1,4,7,5,1,4,3"
-numList = num.split(",")
+num = 600851475143
 
-nums = []
+def isPrime(x):
+    flag = True
+    for i in range(2, x):
+        if (x % i) == 0:
+            flag = False
+    return flag
 
-for x in range(len(numList)):
-    for i in range(len(numList)):
-        n = ""
-        h = i
-        for y in range(x):
-            n += numList[h]
-            h += 1
-            if (len(n) == x):
-                n = int(n)
-                nums.append(n)
-            if (h > len(numList)-1):
-                break
-nums.sort(reverse = True)
-
-def removeDupli():
-    for x in range(len(nums)-1):
-        if nums[x] == nums[x+1]:
-            nums.pop(x+1)
-            removeDupli()
-
-removeDupli()
+for i in range(2, (num // 2)):
+    if isPrime(i) == True:
+        if (num % i) == 0:
+            print(i, "Is a prime factor")
 
 
-
-for x in range(len(nums)):
-    divMax = nums[x] // 2
-    print("check Num", nums[x])
-    for i in range(divMax):
-        if x == 2:
-            break
-        if i > 1:
-            if nums[x] % i == 0:
-                print(nums[x], " Is dividable with", i)
-                break
-        if i == divMax-1:
-            print("Higest is ", nums[x])
-            exit()
